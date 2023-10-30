71002c6d15284ce2e24590f3151df8bb4bf64853
Praveen Singh <108598845+Praveen-Singh0@users.noreply.github.com>
2023-10-30 10:38:31 +0530

    initial commit

---
+++ js/main.js
@@ -0,0 +1,125 @@
+function enlargeImage(image) {
+    var images = document.getElementsByClassName('immge');
+    for (var i = 0; i < images.length; i++) {
+        if (images[i] !== image) {
+            images[i].classList.remove('enlarged-image');
+        }
+    }
+    image.classList.toggle('enlarged-image');
+    
+}
+
+
+
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+// Function to animate the cards when they come into view
+function animateCards() {
+    var cards = document.querySelectorAll('.carad');
+    cards.forEach(function(card) {
+        if (isInViewport(card)) {
+            card.style.animation = 'moveForward 1s ease-in-out';
+            card.style.transform = 'translateX(0)';
+        }
+    });
+}
+
+// Add a scroll event listener to trigger the animation when scrolling
+window.addEventListener('scroll', animateCards);
+
+// Initially, check if cards are in the viewport and trigger the animation
+animateCards();
+
+
+
+
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
